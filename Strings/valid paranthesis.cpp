ddcbe9bc105fc161952cfa575967f08c52b55e47
vanshajsingla <72307225+vanshajsingla@users.noreply.github.com>
2023-04-23 01:04:29 +0530

    Create valid paranthesis.cpp

---
+++ Strings/valid paranthesis.cpp
@@ -0,0 +1,30 @@
+// link: https://leetcode.com/problems/valid-parentheses
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(st.empty())
+                    return false;
+                else if((s[i]==')'&& st.top()=='[')||(s[i]==')'&& st.top()=='{'))
+                    return false;
+                else if((s[i]==']'&& st.top()=='(')||(s[i]==']'&& st.top()=='{'))
+                    return false;
+                else if((s[i]=='}'&& st.top()=='(')||(s[i]=='}'&& st.top()=='['))
+                     return false;
+                else
+                    st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
