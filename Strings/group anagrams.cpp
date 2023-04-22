09943c111857982a37ca6f30b4b58d78e1d54f6e
vanshajsingla <72307225+vanshajsingla@users.noreply.github.com>
2023-04-23 01:02:04 +0530

    Update group anagrams.cpp

--- Strings/group anagrams.cpp
+++ Strings/group anagrams.cpp
@@ -1,4 +1,26 @@
- // approach 2: using 26 size array T.C=O(nk)
+// link: https://leetcode.com/problems/group-anagrams
+// approach 1: using sorting T.C=O(m*nlogn)
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>ans;
+        int n=strs.size();
+        for(int i=0;i<n;i++)
+        {
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(strs[i]);
+        }
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
+/////////////////////////////////////////////////////////////////////////
+// approach 2: using 26 size array T.C=O(nk)
 class Solution {
 public:
 string generate(string word)
